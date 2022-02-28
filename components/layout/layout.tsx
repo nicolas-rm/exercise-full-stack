44e0d777a9706a14ea9884b84483fc8f4db0bdd4
nicolas-rm <nicolas.rm540@gmail.com>
2022-02-27 22:56:52 -0600

    New Nav

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,90 +1,24 @@
-/**
- * TODO: Paqueterias Necesarias para el Nav
- */
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 
-const drawerWidth = 240;
+export default function CenteredTabs() {
+  const [value, setValue] = React.useState(0);
 
-/**
- * * Important: Necesario para que funcione correctamente
- */
-interface Props {
-    window?: () => Window;
-}
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
-export default function ResponsiveDrawer(props: Props) {
-    const { window } = props;
-    const [mobileOpen, setMobileOpen] = React.useState(false);
-
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
-    };
-
-    /**
-     * TODO: Lista principal
-     */
-    const drawer = (
-        <div>
-            <Toolbar />
-            <Divider />
-            <List>
-                {['Bienvenida', 'Menu', 'Platillos'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>
-                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-            {/* <Divider /> */}
-        </div>
-    );
-
-    const container = window !== undefined ? () => window().document.body : undefined;
-
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar position="fixed" sx={{ width: { sm: `calc(100% - ${drawerWidth}px)` }, ml: { sm: `${drawerWidth}px` } }}>
-                <Toolbar>
-                    <IconButton color="inherit" aria-label="open drawer" edge="start" onClick={handleDrawerToggle} sx={{ mr: 2, display: { sm: 'none' } }}>
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" noWrap component="div">
-                        Bienvenidos - Restaurante el Pixel
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }} aria-label="mailbox folders">
-                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-                <Drawer container={container} variant="temporary" open={mobileOpen} onClose={handleDrawerToggle} ModalProps={{
-                    keepMounted: true, /* Better open performance on mobile. */
-                }} sx={{
-                    display: { xs: 'block', sm: 'none' }, '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
-                }}>
-                    {drawer}
-                </Drawer>
-                <Drawer variant="permanent" sx={{ display: { xs: 'none', sm: 'block' }, '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }}} open>
-                    {drawer}
-                </Drawer>
-            </Box>
-
-        </Box>
-    );
+  return (
+    <Box sx={{ width: '100%', bgcolor: 'background.paper', marginTop: 2  }}>
+      <Tabs value={value} onChange={handleChange} centered sx={{ height: '50px',}}>
+        <Tab label="Menu"/>
+        <Tab label="Platillos" />
+        <Tab label="Contacto" />
+      </Tabs>
+      <Divider />
+    </Box>
+  );
 }
