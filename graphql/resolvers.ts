da157ff3c6f38f5a39e3504a943d056f353c86ff
nicolas-rm <nicolas.rm540@gmail.com>
2022-03-01 12:54:16 -0600

    GraphQL

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,6 +1,21 @@
 
-export const reslvers = {
+export const resolvers = {
     Query: {
-        restaurants: () => []
+        restaurants: () => [
+            {
+                name: "El Corazon de Chiapas",
+                adress: "Seccion San Nicolas, M. Nuevo Leon.",
+                id: "8a9020b2-363b-4a4f-ad26-d6d55b51bqes",
+                phone: "XX-XXXX-XXXX",
+                menu: {
+                    create: {
+                        name: "breakfast",
+                        id: "8a9020b2-122d-465v-ad26-d6d55b51bqes",
+                        restaurant: '8a9020b2-363b-4a4f-ad26-d6d55b51bqes',
+                        typeMenu: 'meat_and_chicken'
+                    }
+                }
+            }
+        ]
     }
 }
--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,3 +1,15 @@
-
 import { gql } from 'apollo-server-micro';
 
+export const typeDefs = gql`
+    type Restaurants {
+        name: String,
+        adress: String,
+        id: String,
+        phone: String,
+        menu: [String] 
+    }
+
+    type Query {
+        restaurants: [Restaurants]!
+    }
+`;
